6c7f03d0cee5270e13361985f7444783b4aecec2
Maxim Korotin <36232697+MaximKorotin@users.noreply.github.com>
2021-06-22 20:38:50 +0300

    Вносит правку в функцию setValidityCapacity

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 import'./generator-ads.js';
 import {switchPageState} from './form.js';
-import {validityTitle, validityPrice, validityCapacity} from './validity-form.js';
+import {setValidityTitle, setValidityMaxPrice, setValidityCapacity} from './validity-form.js';
 
 switchPageState(false);
-validityTitle();
-validityPrice();
-validityCapacity();
+setValidityTitle();
+setValidityMaxPrice();
+setValidityCapacity();
